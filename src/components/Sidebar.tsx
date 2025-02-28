2e0d613392bc8cd31a1d41e44a5865ceb7b7a6fa
Aklima-Shampa <shampa.iub@gmail.com>
2025-02-28 14:38:30 +0600

    edited

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -40,18 +40,18 @@ const Sidebar = () => {
       </div>
 
       <div className="space-y-4  mt-4 mr-12">
-        <div className="w-[270px] h-[403px] p-4 m-0 space-y-6 bg-[#C4C4C4]  rounded-lg">
-          <p>Bacola Natural Foods</p>
-          <h3 className="font-semibold mb-2">Special Organic</h3>
-          <h3 className="font-semibold mb-2">Roats Burger</h3>
-          <p className="text-primary font-bold">only-from</p>
-          <span>$14.99</span>
+        <div className="w-[270px] h-[403px] p-4 space-y-3 bg-[#C4C4C4] mt-4 rounded-lg">
+          <p className="font-inter font-light text-white">Bacola Natural Foods</p>
+          <h3 className=" text-xl font-inter font-light text-[#202435]">Special Organic</h3>
+          <h3 className=" text-2xl font-semibold text-[#202435] mb-2">Roats Burger</h3>
+          <p className="text-sm font-light text-[#202435]">only-from</p>
+          <span className="text-3xl  font-dosis font-bold text-[#D51243]">$14.99</span>
         </div>
       </div>
 
       <div className="space-y-4 mt-4 mr-8 ">
         <div className="w-[270px] h-[403px] space-y-6 bg-[#C4C4C4] p-4 rounded-lg text-sm">
-          <h3 className="font-thin mb-2 text-sm">Best bakery products</h3>
+          <h3 className="font-thin mb-2 text-sm ">Best bakery products</h3>
           <h3 className="font-normal mb-2 text-xl text-[#202435]">
             Freshest products
           </h3>
