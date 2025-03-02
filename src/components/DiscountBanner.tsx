1043ae05fd496ee17178228d1989c62b2f9ec7f2
Aklima-Shampa <shampa.iub@gmail.com>
2025-03-03 02:19:38 +0600

    edited

--- src/components/DiscountBanner.tsx
+++ src/components/DiscountBanner.tsx
@@ -1,17 +1,17 @@
 const DiscountBanner = () => {
   return (
     <div>
-      <div className="w-[870px] h-[74px] bg-instacart-rose-light p-4 rounded-lg flex flex-col sm:flex-row items-center justify-between">
+      <div className="w-[870px] h-[74px] bg-instacart-rose-light px-6 rounded-lg flex flex-col sm:flex-row items-center justify-center gap-8">
         <div className="flex items-center">
-          <span className="text-instacart-rose font-medium mr-1">
+          <span className="text-instacart-rose font-regular mr-1">
             Super discount for your
           </span>
-          <span className="text-instacart-rose font-medium underline">
+          <span className="text-instacart-rose font-bold underline">
             first purchase.
           </span>
         </div>
-        <div className="flex items-center mt-2 sm:mt-0">
-          <div className="bg-white border border-gray-200 dotted b-[#FF6048] px-2 py-1 text-sm font-mono mr-2">
+        <div className="flex justify-center items-center mt-2 sm:mt-0">
+          <div className="border-2 border-dotted rounded-full border-[#FF6048] text-[#ED174A] px-2 py-1 text-sm font-bold font-dosis mr-2">
             FREE250AC
           </div>
           <span className="text-xs text-gray-500">
--- src/components/GroceryCategories.tsx
+++ src/components/GroceryCategories.tsx
@@ -12,15 +12,15 @@ const GroceryCategories: React.FC<GroceryCategoriesProps> = ({
   categories,
 }) => {
   return (
-    <div className="grid grid-cols-5 gap-4">
+    <div className="grid grid-cols-5">
       {categories.map((category, index) => (
         <div
           key={index}
-          className={`${category.span} bg-transparent p-4 flex gap-5 `}>
+          className={`${category.span} bg-transparent p-4 flex gap-5 border-2 border-[#E4E5EE]`}>
           <div className="w-16 h-16 bg-gray-200 rounded-sm overflow-hidden flex-shrink-0"></div>
           <div>
-            <h3 className="text-xl font-semibold">{category.name}</h3>
-            <span className="text-sm text-gray-500">
+            <h3 className="text-xm font-semibold text-[#202435]">{category.name}</h3>
+            <span className="text-sm text-[#202435]">
               {category.count} items
             </span>
           </div>
